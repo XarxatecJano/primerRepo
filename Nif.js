f5c5431d2140b0d3937a5f203e5df0033ceda7af
Jano Soler <jano.soler@xarxatecactiva.com>
2021-09-27 12:12:54 +0200

    commit inicial

---
+++ Nif.js
@@ -0,0 +1,28 @@
+class Nif {
+    constructor(numeroDni){
+        this._dni = numeroDni;
+        this._letra = this.calculaLetra();
+    }
+
+    get dni(){
+        return this._dni;
+    }
+
+    set dni(nuevoNumeroDni){
+        this._dni = nuevoNumeroDni;
+        this._letra = this.calculaLetra();
+    }
+
+    calculaLetra(){
+        let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',  'E', 'T'];
+        return letras[this._dni%23];
+    }
+
+    mostrar(){
+        return `${this._dni}-${this._letra}`
+    }
+}
+
+
+
+
